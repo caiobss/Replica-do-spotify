4d7afdd8850b511ac5458c5d163e87a29d0ffa53
Caio Bezerra <68752886+caiobss@users.noreply.github.com>
2022-06-05 17:02:27 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,87 +1,89 @@
-const musicas = [
+//Array
+const musicas =[
     {titulo: 'As You Fade Away',
     artista: 'NEFFEX',
-    src:'songs/As You Fade Away - NEFFEX.mp3',
+    musicasrc:'songs/As You Fade Away - NEFFEX.mp3',
     img:'/imgs/aditya-chinchure-TE50158gdvs-unsplash.jpg'},
 
     {titulo: 'El Secreto',
     artista: 'Yung Logos',
-    src:'songs/El Secreto - Yung Logos.mp3',
+    musicasrc:'songs/El Secreto - Yung Logos.mp3',
     img:'/imgs/shubham-dhage-ykFTt5Dq1RU-unsplash.jpg'},
 
     {titulo: 'Retribution',
     artista: 'NEFFEX',
-    src:'songs/Retribution - NEFFEX.mp3',
+    musicasrc:'songs/Retribution - NEFFEX.mp3',
     img:'/imgs/arthur-ogleznev-OT-tVcdZUMw-unsplash.jpg'},
 
     {titulo: 'Bad Guy',
     artista: 'Billie Eilish',
-    src:'songs/Billie_Eilish_-_bad_guy_(BornMP3.com).mp3',
+    musicasrc:'songs/Billie_Eilish_-_bad_guy_(BornMP3.com).mp3',
     img:'/imgs/billie.jpg'},
 
     {titulo: "Sweet Child O'mine",
     artista: "Guns N' Roses",
-    src:"songs/Sweet Child O' Mine.mp3",
-    img:'/imgs/guns-and-roses-banda.jpg'}
+    musicasrc:"songs/Sweet Child O' Mine.mp3",
+    img:'/imgs/guns-and-roses-banda.jpg'},
+
+    {titulo: 'Killing Strangers',
+    artista: 'Marilyn Manson',
+    musicasrc:'songs/Marilyn Manson - Killing Strangers.mp3',
+    img:'/imgs/Marilyn Manson.jpg'},
+
 ]
 
+//variáveis
+const tempoFinal = document.querySelector('.tempo-fim')
+
 const musica = document.querySelector('audio')
-let indexMusica = 0
-let tempofinal = document.querySelector('.fim')
-let imagem = document.querySelector('img')
-let nomeMusica = document.querySelector('.descricao h2')
-let nomeArtista = document.querySelector('.descricao i')
+const imagem = document.querySelector('.img-musicas img')
+const musicaNome = document.querySelector('.descricoes .nome-musica')
+const Nomeartista = document.querySelector('.descricoes .artista')
+let index = 0
 
-tempofinal.textContent = segudosparaminutos(Math.floor(musica.duration))
-//eventos
-document.querySelector('.botao-play').addEventListener('click', tocarmusica)
+tempoFinal.textContent = minutosEsegundos(Math.floor(musica.duration))
 
-document.querySelector('.botao-pause').addEventListener('click', pausarmusica)
-
-musica.addEventListener('timeupdate', atualizarbarra)
-
-document.querySelector('.anterior').addEventListener('click', () =>{
-    indexMusica--
-    renderizarmusica(indexMusica)
+//eventos e funções
+document.querySelector('.voltar').addEventListener('click', () =>{
+    index--
+    renderizarMusica(index)
 })
 document.querySelector('.proxima').addEventListener('click', () =>{
-    indexMusica++
-    renderizarmusica(indexMusica)
+    index++
+    renderizarMusica(index)
 })
-
-//funções
-function renderizarmusica(index){
-    musica.setAttribute('src', musicas[index].src)
+function renderizarMusica(index){
+    musica.setAttribute('src', musicas[index].musicasrc)
     musica.addEventListener('loadeddata', () =>{
-        nomeMusica.textContent = musicas[index].titulo
-        nomeArtista.textContent = musicas[index].artista
+        musicaNome.textContent = musicas[index].titulo
+        Nomeartista.textContent = musicas[index].artista
         imagem.src = musicas[index].img
-        tempofinal.textContent = segudosparaminutos(Math.floor(musica.duration))
+        tempoFinal.textContent = minutosEsegundos(Math.floor(musica.duration))
     })
 }
-
-function tocarmusica(){
+document.querySelector('.buttons .play').addEventListener('click', () =>{//evento de ativação da musica
     musica.play()
-    document.querySelector('.botao-pause').style.display = 'block'
-    document.querySelector('.botao-play').style.display = 'none'
-}
-function pausarmusica(){
+    document.querySelector('.buttons .play').style.display = 'none'
+    document.querySelector('.buttons .pause').style.display = 'block'
+})
+document.querySelector('.buttons .pause').addEventListener('click', () =>{//evento do pause da musica
     musica.pause()
-    document.querySelector('.botao-pause').style.display = 'none'
-    document.querySelector('.botao-play').style.display = 'block'
-}
-function atualizarbarra(){
-    let barra = document.querySelector('progress')
-    barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) +'%'
-    let tempodecorrido = document.querySelector('.inicio')
-    tempodecorrido.textContent = segudosparaminutos(Math.floor(musica.currentTime))
-}//a propriedade math.floor arredonda um numero decimal para baixo ou seja: 2.2.3.4 = 2
+    document.querySelector('.buttons .play').style.display = 'block'
+    document.querySelector('.buttons .pause').style.display = 'none'
+})
+musica.addEventListener('timeupdate', () =>{//atualização em tempo real da musica para a barra de progresso e timer
+    let progresso = document.querySelector('progress')
+    progresso.style.width = (musica.currentTime / musica.duration) * 100 + '%'
+    let tempo = document.querySelector('.tempo-inicio')
+    tempo.textContent = minutosEsegundos(Math.floor(musica.currentTime))
+})
 
-function segudosparaminutos(segundos){
-    let campominutos = Math.floor(segundos / 60)
-    let camposegudos = (segundos % 60)
-    if (camposegudos < 10){
-        camposegudos = '0' + camposegudos
+function minutosEsegundos(segundos){//funcao para definicao e limetes de minutos e segundos
+    let tempoMinutos = Math.floor(segundos / 60)
+    let tempoSegudos = segundos % 60
+    if (tempoSegudos < 10){
+        tempoSegudos ='0' +tempoSegudos
     }
-    return campominutos+':'+camposegudos
+    return (tempoMinutos+ ':' +tempoSegudos)
+
 }
